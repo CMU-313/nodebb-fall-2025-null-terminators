f66f6e531ae18b6da2186ed81b99d868624c9cf6
Alexander Torres Vivaldo <atorresv@andrew.cmu.edu>
2025-10-08 17:16:25 +0000

    Improved security of getTopicsByDate helper method to check for user authorization

--- src/topics/filtering.js
+++ src/topics/filtering.js
@@ -1,32 +1,49 @@
 'use strict';
 
 const privileges = require('../privileges');
-const db = require('../database');
+const db = require('../database');	
 
 module.exports = function (Topics) {
 	Topics.getTopicsByDate = async function ({date, uid, cid}) {
+		// Check for Valid inputs
+		if (cid === undefined || cid === null) {
+			throw new Error('[[error:invalid-cid]]');
+		}
+
+		if (uid === undefined || uid === null) {
+			throw new Error('[[error:invalid-uid]]');
+		}
+
 		// Check for valid date format
 		if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
 			throw new Error('Invalid date format. Use YYYY-MM-DD.');
+		}
+
+		// Check if user is authorized to read from the category
+		const canRead = await privileges.categories.can('categories:read', cid, uid);
+
+		if (!canRead) {
+			throw new Error('[[error:no-privileges]]');
 		}
 
 		// Convert date to timestamp range
 		const startTimestamp = new Date(`${date}T00:00:00Z`).getTime();
 		const endTimestamp = new Date(`${date}T23:59:59Z`).getTime();
 
-		// Query DB for topics within the date range
-		let tids = await db.getSortedSetRangeByScore('topics:tid', 0, -1, startTimestamp, endTimestamp);
+		const categoryTids = await db.getSortedSetMembers(`cid:${cid}:tids`);
+		const allTidsInRange = await db.getSortedSetRangeByScore(
+			'topics:tid',
+			0,
+			-1,
+			startTimestamp,
+			endTimestamp,
+		);
 
-		// If the category is provided, filter topics by category
-		if (cid) {
-			const categoryTids = await db.getSortedSetMembers(`cid:${cid}:tids`);
-			tids = tids.filter(tid => categoryTids.includes(tid));
-		}
+		// filter topics to only those in specified category & date range
+		let tids = allTidsInRange.filter(tid => categoryTids.includes(tid));
 
-		// Filter by privileges if uid is provided
-		if (uid) {
-			tids = await privileges.topics.filterTids('topics:read', tids, uid);
-		}
+		// Topic privelege check
+		tids = await privileges.topics.filterTids('topics:read', tids, uid);
 
 		return await Topics.getTopicsByTids(tids);
 
