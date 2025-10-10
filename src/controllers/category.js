57ceecc8290ecce3b2182fad395a1ca39530c1d8
dsblack0 <sduggira@andrew.cmu.edu>
2025-10-10 21:17:42 +0000

    return search term if exists to template instead of boolean

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -110,13 +110,15 @@ categoryController.get = async function (req, res, next) {
 		return next();
 	}
 
+	// Return search term to template if exists, otherwise null
+	categoryData.searchTerm = searchTerm || null;
+
 	// Run search to return only matched topics when search term is present
 	if (searchTerm) {
 		const searchResults = await topics.searchInCategory(searchTerm, cid, req.uid);
 		// Slicing array of results for pagination - get first page (from Copilot)
 		categoryData.topics = searchResults.slice(start, stop + 1);
 		categoryData.topic_count = searchResults.length;
-		categoryData.isSearchResults = true;
 	}
 
 	if (topicIndex > Math.max(categoryData.topic_count - 1, 0)) {
