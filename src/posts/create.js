6cd1de09fbe7d1131b14c4af4a6685c96bbd36f6
Sofia Yu <nihansofia0127@gmail.com>
2025-10-10 23:46:24 +0000

    fixed linter error due to merging conflicts

--- src/posts/create.js
+++ src/posts/create.js
@@ -35,7 +35,7 @@ module.exports = function (Posts) {
 		let postData = {
 			pid, uid, tid, content, sourceContent, timestamp,
 			visibleTo: JSON.stringify(validatedVisibleTo),
-      anonymous: !!data.anonymous
+			anonymous: !!data.anonymous,
 		};
 
 		if (data.toPid) {
