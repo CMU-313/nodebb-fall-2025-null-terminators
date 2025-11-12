3d7a8b09861d34a50955a3439fbb2be83210c413
justsomeboy26 <zhangjimmy31@gmail.com>
2025-11-12 04:32:27 +0000

    Linter Correction

--- src/translate/index.js
+++ src/translate/index.js
@@ -5,8 +5,8 @@
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-	const TRANSLATOR_API = 'http://128.2.221.60:5000'
-	const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
+	const TRANSLATOR_API = 'http://128.2.221.60:5000';
+	const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
 	const data = await response.json();
 	return [data.is_english, data.translated_content];
 };
